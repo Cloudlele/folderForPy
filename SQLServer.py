6eac602c87dc65923f8202730e19f396539ae99f
Cloudlele <omikula123@gmail.com>
2018-11-17 20:00:09 +0200

    Serv
    Signed-off-by: Cloudlele <omikula123@gmail.com>

--- SQLServer.py
+++ SQLServer.py
@@ -1,22 +1,16 @@
 import pypyodbc
-from tkinter import *
+import json
 
-root = Tk()
 connection = pypyodbc.connect(driver='{SQL Server}', server='DESKTOP-7GE22QK\SQLEXPRESS', database='TravelAgency')
 cursor = connection.cursor()
 
-hotel = input('Enter hotel')
 
-SQLQuery = ("""Select Tours.Code_Client, Tours.Code_Employee, Tours.StartDate, Tours.EndDate, Tours.Duration, Tours.Code_Vyg, TypesOfRest.Name, Tours.Code_Hotel, Hotels.Name
-From Tours
-Join TypesOfRest On Tours.Code_Vyg = TypesOfRest.CodeVyg
-Join Hotels On Tours.Code_Hotel = Hotels.Code_Hotel
-Where Hotels.Name = """ + "'" + hotel + "'")
+SQLQuery = ("""Select Employees.PIP, Employees.Years, Employees.Phone
+From Employees""")
 
 cursor.execute(SQLQuery)
-
 result = cursor.fetchall()
-
+json.dumps(result)
 print(result)
 
 connection.close()
