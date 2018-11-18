56f4c7b6bebd5f863f98cb12b12a79b9d24fd5e3
Cloudlele <omikula123@gmail.com>
2018-11-18 23:35:53 +0200

    Serv
    Signed-off-by: Cloudlele <omikula123@gmail.com>

--- ServerPy3.py
+++ ServerPy3.py
@@ -1,19 +1,39 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-import time, socket, json
+import time, socket
+import pypyodbc
+import json
 
 hostName = socket.gethostbyname(socket.gethostname())
 hostPort = 9000
+dataDic = dict()
 
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
+        if self.path == '/data':
+            connection = pypyodbc.connect(driver='{SQL Server}', server='DESKTOP-7GE22QK\SQLEXPRESS',
+                                          database='TravelAgency')
+            cursor = connection.cursor()
+            SQLQuery = ("""Select Employees.PIP, Employees.Years, Employees.Phone
+                    From Employees""")
+            cursor.execute(SQLQuery)
+            result = cursor.fetchall()
+            connection.close()
+            name = list()
+            year = list()
+            phone = list()
+            for i in result:
+                name.append(str(i[0]))
+                year.append(str(i[1]))
+                phone.append(str(i[2]))
+            dataDic['Pip'] = name
+            dataDic['Years'] = year
+            dataDic['Phone'] = phone
+            print(dataDic)
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(bytes("<html><head><title>Title goes here.</title></head>", "utf-8"))
-        self.wfile.write(bytes("<body><p>This is a test.</p>", "utf-8"))
-        self.wfile.write(bytes("<p>You accessed path: %s</p>" % self.path, "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8"))
+        self.wfile.write(bytes("<p>%s</p>" % dataDic, "CP1251"))
 
     def do_POST(self):
         content_len = int(self.headers['Content-Length'])
@@ -25,8 +45,6 @@ class MyServer(BaseHTTPRequestHandler):
                 iter = id
             if id == iter+1:
                 print(i)
-
-
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
